c33494152711e34cd2be332880447998422e896b
“[Amri Iqra Samudra Al-Fatihah <amriiqosamudraalfatihah@gmail.com>
2025-10-10 16:04:18 +0700

    ubah harga mesin

--- config/machine_upgrade.php
+++ config/machine_upgrade.php
@@ -2,31 +2,31 @@
 
 return [
     // Mesin Pemotong & Bending Pipa
-            1 => [
-                'upgrade_prices' => [2 => 3250, 3 => 4000],
-                'capacity_per_level' => [1 => 5, 2 => 6, 3 => 8],
-                'time_per_level' => [1 => 5, 2 => 4, 3 => 3],
-                'sell_prices' => [1 => 1500, 2 => 2000  , 3 => 2500],
-            ],
-            // Mesin Las Otomatis
-            2 => [
-                'upgrade_prices' => [2 => 3500, 3 => 4250],
-                'capacity_per_level' => [1 => 7, 2 => 8, 3 => 9],
-                'time_per_level' => [1 => 7, 2 => 6, 3 => 5],
-                'sell_prices' => [1 => 2000, 2 => 2500, 3 => 3000],
-            ],
-            // Mesin Pengecatan & Oven
-            3 => [
-                'upgrade_prices' => [2 => 4500, 3 => 5000],
-                'capacity_per_level' => [1 => 4, 2 => 5, 3 => 6],
-                'time_per_level' => [1 => 6, 2 => 6, 3 => 5],
-                'sell_prices' => [1 => 2000, 2 => 2500, 3 => 2750],
-            ],
-            // Mesin Perakitan
-            4 => [
-                'upgrade_prices' => [2 => 4500, 3 => 6000],
-                'capacity_per_level' => [1 => 7, 2 => 9, 3 => 9],
-                'time_per_level' => [1 => 4, 2 => 3, 3 => 2],
-                'sell_prices' => [1 => 2000, 2 => 2500, 3 => 3000],
-            ],
+    1 => [
+        'upgrade_prices' => [2 => 3250, 3 => 4000],
+        'capacity_per_level' => [1 => 5, 2 => 6, 3 => 8],
+        'time_per_level' => [1 => 5, 2 => 4, 3 => 3],
+        'sell_prices' => [1 => 2500, 2 => 3250, 3 => 4000],
+    ],
+    // Mesin Las Otomatis
+    2 => [
+        'upgrade_prices' => [2 => 3500, 3 => 4250],
+        'capacity_per_level' => [1 => 7, 2 => 8, 3 => 9],
+        'time_per_level' => [1 => 7, 2 => 6, 3 => 5],
+        'sell_prices' => [1 => 3000, 2 => 3500, 3 => 4250],
+    ],
+    // Mesin Pengecatan & Oven
+    3 => [
+        'upgrade_prices' => [2 => 4500, 3 => 5000],
+        'capacity_per_level' => [1 => 4, 2 => 5, 3 => 6],
+        'time_per_level' => [1 => 6, 2 => 6, 3 => 5],
+        'sell_prices' => [1 => 3250, 2 => 4500, 3 => 5000],
+    ],
+    // Mesin Perakitan
+    4 => [
+        'upgrade_prices' => [2 => 4500, 3 => 6000],
+        'capacity_per_level' => [1 => 7, 2 => 9, 3 => 9],
+        'time_per_level' => [1 => 4, 2 => 3, 3 => 2],
+        'sell_prices' => [1 => 3500, 2 => 4500, 3 => 6000],
+    ],
 ];
